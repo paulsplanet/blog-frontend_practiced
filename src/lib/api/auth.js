9cb187f7e866920835111dccc5a56df4acdad535
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-08-20 15:03:41 -0700

    middleware

---
+++ src/lib/api/auth.js
@@ -0,0 +1,10 @@
+import client from "./client";
+
+// LogIn
+export const login = ({ username, password }) => client.post('/api/auth/login', { username, password });
+
+// Register
+export const register = ({ username, password }) => client.post('/api/auth/register', { username, password });
+
+// Check Login Status
+export const check = () => client.get('/api/auth/check');
---
+++ src/lib/api/client.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const client = axios.create();
+
+/*
+    global setup example
+    // use API address
+    client.defaults.baseURL = 'https://external-api-server.com/'
+
+    // header setup
+    client.defaults.headers.common['Authorization'] = 'Bearer a1b2c3d4';
+
+    // intercepter setup
+    axios.intercepter.response.use({
+        response => {
+            // when it successed
+            return response;
+        },
+        error => {
+            // when it failed
+            return Promise.reject(error);
+        })
+    })
+*/
+
+export default client;
